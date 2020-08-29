fde9d8354c3851afef0a27534c6c1b84e7800bb3
Mabel Lawrence <mabellawrence@Mabels-MacBook-Pro-2.local>
2020-08-29 19:32:21 -0400

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-2",
+    BUCKET: "notes-application-attachments"
+  },
+  apiGateway: {
+    REGION: "us-east-2",
+    URL: "https://8japt8yjyl.execute-api.us-east-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-2",
+    USER_POOL_ID: "us-east-2_wSswHRprv",
+    APP_CLIENT_ID: "4anu7f62h0e2jrtiuqrec3vtt3",
+    IDENTITY_POOL_ID: "us-east-2:041402fe-14cf-4694-8c2c-020285862889"
+  }
+};
